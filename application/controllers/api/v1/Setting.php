260ecf5c5732d34efa38bd4e4dd5d41cf8259f52
Kelvin More <62706940+kelvinmore@users.noreply.github.com>
2020-04-21 05:06:22 -0400

    Update Setting.php

--- application/controllers/api/v1/Setting.php
+++ application/controllers/api/v1/Setting.php
@@ -9,12 +9,55 @@
 		}
 		function index_get($func){
 			
-
+			switch ($func) {
+				case 'get_assets':
+					$data = $this->get_assets();
+					break;
+				
+				default:
+					# code...
+					break;
+			}
+			$this->response($data, REST_Controller::HTTP_OK);
 		}
 		function index_post($func){
-
+			switch ($func) {
+				case 'save_setting':
+					$data = $this->save_setting();
+					break;
+				
+				default:
+					# code...
+					break;
+			}
+			$this->response($data, REST_Controller::HTTP_OK);
 
 		}
 
+		function get_assets(){
+			$data = [];
+			$query = $this->db->get("tbl_bodytype");
+			$data["bodytypes"] = $query->result_array();
+			$query = $this->db->get("tbl_ethnicity");
+			$data['ethnicitys'] = $query->result_array();
+			$query = $this->db->get("tbl_nationality");
+			$data['nationalitys'] = $query->result_array();
+			$query = $this->db->get("tbl_orientations");
+			$data["orientations"] = $query->result_array();
+			$query = $this->db->get("tbl_religion");
+			$data["religions"] = $query->result_array();
+			$query = $this->db->get("tbl_status");
+			$data['statuses'] = $query->result_array();
+			return $data;
+		}
+		function save_setting(){
+			$data = $this->input->post();
+			$userId = $data['userId'];
+			unset($data['userId']);
+			$this->db->where("userId", $userId);
+			$query = $this->db->update("tbl_users", $data);
+			if($query) return array("success"=>1);
+			else return array("success"=>0);
+		}
 	}
 ?>
